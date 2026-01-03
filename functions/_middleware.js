7b690c8abd2c543c38d9d26e11957afedc8ddbac
roublaaa <roblaaa04@gmail.com>
2026-01-03 02:59:22 +0100

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,5 +1,15 @@
-export function onRequest() {
-  return new Response("MIDDLEWARE OK", {
-    headers: { "Content-Type": "text/plain" }
+export function onRequest(context) {
+  const url = new URL(context.request.url);
+  const token = url.searchParams.get("token");
+
+  if (token === "MONTH123") {
+    return context.next();
+  }
+
+  return new Response(`
+    <h2>⛔ الدخول فقط للمشتركين</h2>
+    <a href="https://wa.me/212XXXXXXXXX">واتساب</a>
+  `, {
+    headers: { "Content-Type": "text/html" }
   });
 }
